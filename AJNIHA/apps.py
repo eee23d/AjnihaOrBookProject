5b96e6332c51ce1de8557994ae648ec62d7c88ad
mlooksabouni <59798883+mlooksabouni@users.noreply.github.com>
2021-05-17 13:37:50 +0300

    Update apps.py
    change the comment

--- AJNIHA/apps.py
+++ AJNIHA/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
-#new change
+#new change hi i change that thanks
 class AjnihaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'AJNIHA'
