fd8936a5ad28038c60a2c31721446197cab51303
malak <reembayazid@outlock.com>
2021-05-30 03:48:49 +0300

    تصحيح ما لاحظته من مشكاكل مع الجوالات

--- AJNIHA/models.py
+++ AJNIHA/models.py
@@ -9,8 +9,7 @@ from django.contrib.auth.models import User
 def create_auto_sheleves(sender, instance, created, **kwargs):
     if created:
         ReaderAccount.objects.create(fName=instance.first_name, sName=instance.last_name,
-                                            username=instance, email=instance.email, Gender="",
-                                            country="", password=instance.password, birthday=None)
+                                            username=instance, email=instance.email)
         print('user account created!')
 
 class Book(models.Model):
@@ -22,11 +21,6 @@ class Book(models.Model):
     image = models.ImageField(default='/book.png',blank=True,null=True,auto_created=True,upload_to='')
     def __str__(self):
         return self.bookTitle
-
-GENDER_CHOICES = (
-   ('M', 'Male'),
-   ('F', 'Female')
-)
 
 class ReaderAccount(models.Model):
 
